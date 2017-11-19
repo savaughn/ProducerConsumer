d04143bed65a4a589e5f6090b39ee3f1614d4503
savaughn <svaughn57@gmail.com>
2017-11-19 01:36:17 -0600

    system exit and comments

--- src/ProducerConsumer.java
+++ src/ProducerConsumer.java
@@ -9,6 +9,7 @@ import java.util.Random;
 
 public class ProducerConsumer { 
 	
+/***************     Buffer     *******************/	
 	public static class Buffer{
 		
 		private final int bufferSize = 5;
@@ -31,8 +32,9 @@ public class ProducerConsumer {
 			full = new Semaphore(0); 
 		}
 		
+		// Producer call in buffer
 		public void produce(int randInt) {
-			while (count == bufferSize) {};
+			while (count == bufferSize) {};  //when buffer is full wait
 			
 			System.out.println("Buffer count: " + count);
 			try {
@@ -43,17 +45,16 @@ public class ProducerConsumer {
 			++count;
 			buffer[bufferIn] = randInt;
 			System.out.println("Producer: " +randInt);
-			bufferIn = (bufferIn + 1)% bufferSize;
-			
-			
+			bufferIn = (bufferIn + 1)% bufferSize;			
 			mutex.release();
 			full.release();
 		}
 		
+		//consumer call in buffer
 		public int consume() {
 			
 			int bufferRead = 0;
-			while (count == 0) {};
+			while (count == 0) {};  //when buffer is empty wait
 			
 			try {
 				full.acquire();
@@ -71,6 +72,11 @@ public class ProducerConsumer {
 		}
 			
 	}
+
+/***********************     Producer     *****************************
+ * 	Fills buffer with random integers.								  *
+ *	100 iterations with random sleep time between 0 and 0.5 seconds	  *
+ **********************************************************************/
 
 	public static class Producer implements Runnable {
 		
@@ -97,7 +103,12 @@ public class ProducerConsumer {
 			}
 		}		
 	}
-	
+
+/************************     Consumer     **********************************
+ *  Returns random integer stored in buffer by producer.					*
+ *	100 iterations with random sleep time between 0 and 0.5 seconds.		*
+ ****************************************************************************/
+
 	public static class Consumer implements Runnable {
 		
 		private Buffer buffer;
@@ -126,30 +137,38 @@ public class ProducerConsumer {
 	
 	public static void main(String args[]) throws InterruptedException {
 		
-		//parse args
+		//parse arguments
 		int sleep = Integer.parseInt(args[0]);
 		int producerThreadCount = Integer.parseInt(args[1]);
 		int consumerThreadCount = Integer.parseInt(args[2]);
 		
-		//Create buffer
+		//Instantiate buffer
 		Buffer buffer = new Buffer();
 		
+		//Instantiate thread intArrays
 		Thread producers[] = new Thread[producerThreadCount];
 		Thread consumers[] = new Thread[consumerThreadCount];
 		
+		//Create producer threads
 		for(int i=0; i < producerThreadCount; i++) {
 			producers[i] = new Thread(new Producer(buffer));
 			producers[i].start();
 		}
 		
+		//Create consumer threads
 		for(int i = 0; i < consumerThreadCount; i++) {
 			consumers[i] = new Thread(new Consumer(buffer));
 			consumers[i].start();
 		}	
 		
+		//Sleep for specified time while threads modify buffer
 		try {
 			Thread.sleep(sleep*1000);
 		} catch(InterruptedException e) {}
+		
+		//Exit after sleep
+		System.exit(0);
+		
 	}	
 			
 }
